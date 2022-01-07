12afe72c5e8e2c207070294b50432d800574df89
audrey <mbozorgi604@gmail.com>
2022-01-07 18:15:23 +0330

    add input in create game screen to gather usernames

--- src/screens/CreateGame.jsx
+++ src/screens/CreateGame.jsx
@@ -1,28 +1,37 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
-import { useUserContext } from '../hooks/useUserContext'
+import { Form, Container, Row, Col } from 'react-bootstrap'
 import boardInBackground from '../assets/images/board_background.png';
 
 const CreateGame = props => {
-    const user = useUserContext()
 
     return (
-        <Container>
-            <section className='section'>
-                <span>Enter your friends username</span>
-                <span>to ask him/her to join</span>
+        <StyledContainer>
+            <Row>
+                <Col>
+                <section className='section'>
+                <span className='sectionBigText'>Enter your friends username</span>
+                <span className='sectionMediumText'>To join the game</span>
+                <div className='formWrapper'>
+                    <StyledFormControl type="text" placeholder="Enter Username" />
+                </div>
             </section>
             <section className='section'>
                 create sss
             </section>
-        </Container>
+                </Col>
+            </Row>
+            
+        </StyledContainer>
     )
 }
 
 export default CreateGame
 
 
-const Container = styled.div`
+
+
+const StyledContainer = styled(Container)`
     height: 100vh;
     background-image: url(${boardInBackground});
     .section{
@@ -31,5 +40,21 @@ const Container = styled.div`
         align-items:center;
         flex-direction: column;
         justify-content: center;
+        .sectionBigText{
+            font-size: 20px;
+            font-weight: 900
+        }
+        .sectionMediumText{
+            font-size: 18px;
+            font-weight: 700
+        }
+        .formWrapper{
+            width: 100%;
+            padding: 7px;
+            margin-top: 20px
+        }
     }
+`
+const StyledFormControl = styled(Form.Control)`
+    
 `
