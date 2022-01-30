b47f0c81500a26f4321d988f75bcd048e0be369f
audrey <mbozorgi604@gmail.com>
2022-01-30 19:49:31 +0330

    code clean up

--- src/screens/CreateGame.jsx
+++ src/screens/CreateGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Form, Container, Row, Col } from 'react-bootstrap'
 import boardInBackground from '../assets/images/board_background.png';
