abb0018048aad914a05937585d90119caaaca955
audrey <mbozorgi604@gmail.com>
2022-02-01 01:47:29 +0330

    join to game api added

--- src/api/game.js
+++ src/api/game.js
@@ -4,4 +4,8 @@ export const createNewGame = async () => {
     return await fetchAPI({ url: '/game/create-new', method: 'POST' })
 }
 
+export const joinToGame = async () => {
+    return await fetchAPI({ url: '/game/join', method: 'PUT' })
+}
+
 
