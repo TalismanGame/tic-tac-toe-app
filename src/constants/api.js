4811ba59acd47051018e20982faae8f08b38ea53
audrey <mbozorgi604@gmail.com>
2022-02-28 00:09:44 +0330

    remove port as it has conflict with what I defined in nginx

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ let baseURL = ''
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:8000/api/v1'
 } else {
-    baseURL = 'http://194.5.192.251:8000/api/v1'
+    baseURL = 'http://194.5.192.251/api/v1'
 }
 
 export { baseURL }
