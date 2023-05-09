0743fb9f9d8eb55b15996627a3070830255f834c
audrey <mbozorgi604@gmail.com>
2023-05-09 18:15:35 +0330

    web server ip updated

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ let baseURL = ''
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:8000/api/v1'
 } else {
-    baseURL = 'http://130.185.120.108/api/v1'
+    baseURL = 'http://46.101.208.155:8000/api/v1'
 }
 
 export { baseURL }
