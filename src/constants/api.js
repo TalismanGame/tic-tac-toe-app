e6d39a9c90a15f016c31cecdbbc371a14ef26547
audrey <mbozorgi604@gmail.com>
2023-05-09 19:56:00 +0330

    fix api

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ let baseURL = ''
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:8000/api/v1'
 } else {
-    baseURL = 'http://talismangame.ir/api/v1'
+    baseURL = 'http://46.101.208.155/api/v1'
 }
 
 export { baseURL }
